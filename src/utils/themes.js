ba26f419daa4d97967d9054ecb02b76fe50f448d
jenross <ross.m.jennifer@gmail.com>
2020-05-20 19:50:19 -0400

    added theme object

---
+++ src/utils/themes.js
@@ -0,0 +1,12 @@
+import { primary, primaryDark, neutral } from "./colors";
+import { primaryFont } from "./typography";
+
+export const defaultTheme = {
+    primaryPurple: primary[100],
+    primaryPurpleHover: primaryDark[100], 
+    primaryPurpleActive: primaryDark[100],
+    textColorOnPrimary: primary[400],
+    textColor: neutral[600], 
+    textColorInverted: primary[100], 
+    primaryFont
+}
