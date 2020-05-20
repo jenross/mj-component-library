d5a93109bdc6ba04ff8ae40285ecf966b10615e4
jenross <ross.m.jennifer@gmail.com>
2020-05-20 19:45:23 -0400

    added primaryfont and typescale

---
+++ src/utils/typography.js
@@ -0,0 +1,12 @@
+export const primaryFont = '"Raleway", serif'; 
+
+export const typeScale = {
+    header1: "7.594rem",
+    header2: "5.063rem",
+    header3: "3.375rem",
+    header4: "2.25rem",
+    header5: "1.5rem",
+    paragraph: "1rem",
+    helperText: "0.8rem",
+    copyrightText: "0.7rem"
+  };
